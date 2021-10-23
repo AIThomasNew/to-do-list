f46f620ab4d6f757bab79badf74b0bbdfd1718c9
Thomas <thomas.new.it@gmail.com>
2021-10-23 20:44:45 +0500

    hooks

--- App.js
+++ App.js
@@ -1,15 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { AddTodo } from './src/AddTodo.js';
 import { Navbar } from './src/Navbar.js';
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (title) => {
+    // const newTodo = {
+    //   id: Date.now().toString(),
+    //   title: title,
+    // };
+
+    // setTodos(todos.concat([newTodo]));
+    // setTodos((prevTodos) => {
+    //   return [...prevTodos, newTodo];
+    // });
+
+    setTodos((prev) => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title: title,
+      },
+    ]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Navbar title="Todo App!" />
+    <View>
+      <Navbar title="Todo App" />
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+  },
 });
--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { View, StyleSheet, TextInput, Button } from 'react-native';
 
-export const AddTodo = (props) => {
-  return;
-  <View style={styles.block}></View>;
+export const AddTodo = ({ onSubmit }) => {
+  return (
+    <View style={styles.block}>
+      <TextInput style={styles.input} />
+      <Button title="Добавить" />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-  block: {},
+  block: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    // marginLeft: 10,
+    // marginRight: 10,
+  },
+  input: {
+    width: '70%',
+    padding: 10,
+    borderStyle: 'solid',
+    borderBottomWidth: 2,
+    borderBottomColor: '#3949ab',
+  },
 });
