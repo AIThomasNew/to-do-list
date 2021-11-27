4d7720248f0002111cbaa1e96bef1f69fba37ea9
Thomas <thomas.new.it@gmail.com>
2021-11-27 22:57:51 +0500

    инструкция

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -15,8 +15,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    // marginLeft: 10,
-    // marginRight: 10,
   },
   input: {
     width: '70%',
